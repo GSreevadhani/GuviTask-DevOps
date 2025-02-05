b922154b2cc6c6f46000859eb824604515e76efd
G.SREEVADHANI <127740281+GSreevadhani@users.noreply.github.com>
2025-02-05 11:28:39 +0530

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -2,8 +2,6 @@
 echo hi123
 chmod +x build.sh
 ./build.sh
-docker login -u sreevadhani -p "$(cat docker_password.txt)"
+docker login -u sreevadhani -p "$DOCKER_PASSWORD"
 docker tag test sreevadhani/ar
 docker push sreevadhani/ar
-
-    
