ab6f8211f992c70b07dec7cdec91966470abec8b
G.SREEVADHANI <127740281+GSreevadhani@users.noreply.github.com>
2025-02-06 13:52:30 +0530

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -11,5 +11,6 @@ docker tag test sreevadhani/ar
 docker push sreevadhani/ar  
 
 docker pull sreevadhani/ar
-
-docker run -d -p 8000:80 --name test sreevadhani/ar
+docker run -d -p 8000:80 --name=my-app-repo sreevadhani/ar
+kubectl create deployment ar-deploy --image=sreevadhani/ar --port 80
+kubectl expose deployment ar-deploy --type=NodePort --port=80
